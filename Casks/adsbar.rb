e14451ff80b693f177a33a21f346c46ea0d40bee
Thomas Dietrich <dietrich.tom@gmail.com>
2026-04-14 19:54:57 +0200

    chore: bump adsbar to v1.1

--- Casks/adsbar.rb
+++ Casks/adsbar.rb
@@ -1,6 +1,6 @@
 cask "adsbar" do
-  version "1.0"
-  sha256 "24f4ded3ff9a5ae2098e1c351bc11029f1c5f9f8372f4fd2b0698b46190735b4"
+  version "1.1"
+  sha256 "5b2cf1847c7fc809432c484e53679ce827f60c80b0312556f91c8d462d238f32"
 
   url "https://github.com/ciruz/adsbar/releases/download/v#{version}/ADSBar.zip"
 
