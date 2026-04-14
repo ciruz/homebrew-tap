68cfc90c27293f66d86ac08058fd3d16fccd30bb
Thomas Dietrich <dietrich.tom@gmail.com>
2026-04-14 14:40:18 +0200

    chore: bump minebar to v1.1

--- Casks/minebar.rb
+++ Casks/minebar.rb
@@ -1,6 +1,6 @@
 cask "minebar" do
-  version "1.0"
-  sha256 "8ce4c2b1ebd4ad45c0d1fade8da077cb3ad4607ee0333536b177264b66b5991d"
+  version "1.1"
+  sha256 "2bc6585e2e423d8d9ff646db64ff421fc4ef43a5190e468a45221906e4f53248"
 
   url "https://github.com/ciruz/minebar/releases/download/v#{version}/MineBar.zip"
 
