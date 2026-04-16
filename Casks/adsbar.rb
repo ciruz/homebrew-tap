1bbdcbdf61b79cd04147b29038cdbf91ca1a75d2
Thomas Dietrich <dietrich.tom@gmail.com>
2026-04-16 08:12:17 +0200

    chore: adsbar correct checksum

--- Casks/adsbar.rb
+++ Casks/adsbar.rb
@@ -1,6 +1,6 @@
 cask "adsbar" do
   version "1.2"
-  sha256 "cf8a51be2a5b82b5addedb30f27c9601e27134d21f37a91d5e5a9294d49f93d4"
+  sha256 "4f34be978fb9c24efbba0cbbbd03d1e77d9037ac949b80657516abdcffc2e4b9"
 
   url "https://github.com/ciruz/adsbar/releases/download/v#{version}/ADSBar.zip"
 
